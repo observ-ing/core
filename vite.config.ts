6030aafa749cab5eb4f8d87811741773802f93c0
Corey Farwell <coreyf@rwell.org>
2026-01-14 21:20:57 -0500

    bump node; fix vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyDir: true,
+  },
+});
